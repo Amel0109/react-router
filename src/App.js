4962cacd8e85ee8635969274c0e5827b05f5d1c5
Amel0109 <amel.mahmic@gmail.com>
2021-04-03 17:07:19 +0200

    Admin init setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Admin } from '../src/pages/admin/Admin';
 
 
 function App() {
---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Admin.css';
+
+function Admin() {
+    return (
+        <div className="Admin">
+            Admin
+        </div>
+    )
+};
+
+export { Admin };
